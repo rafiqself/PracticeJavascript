aedb4f6fa6e3d3c05bf811694f8774ca4051193d
Mohammed Rafiq <rafiq.ace5@gmail.com>
2023-08-15 11:47:57 +0530

    Freeze and Seal

---
+++ Freeze_Seal.js
@@ -0,0 +1,15 @@
+const arr = [1,2,3,4];
+Object.freeze(arr);
+// arr.push(5)
+
+let user = {
+    firstName: "Mohammed"
+}
+Object.seal(user);
+Object.isSealed(user)
+user.lastName = "Rafiq"
+
+user.age = 30;
+// console.log(user.firstName)
+console.log(user)
+
